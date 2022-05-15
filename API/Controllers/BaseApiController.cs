4567e954ceb8e1988947e0a1e90485c9622927dd
Sean Elias <sean.s.elias@outlook.com>
2022-05-15 23:16:47 +1000

    Added users controller

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,20 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        protected ActionResult HandleResults<T>(Result<T> result)
+        {
+            if(result == null) return NotFound();
+            if(result.IsSuccess && result.Value != null)
+                return Ok(result.Value);
+            if(result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
+    }
+}
